82afd2151fad6e5f02662a58836c675d42b7d908
NityaNepal <159408073+NityaNepal@users.noreply.github.com>
2024-02-19 14:08:34 +0600

    Add files via upload

---
+++ first.py
@@ -0,0 +1,11 @@
+print("hello world")
+print("i love coding")
+grade = 90
+if grade >= 90:
+    print("Grade A")
+elif grade >= 80:
+    print("Grade B")
+elif grade >= 70:
+    print("Grade C")
+else :
+    print("Grade D")
