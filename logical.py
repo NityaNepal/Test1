bb3d40b9e015280ed79a871681fa01ebd5418abf
NityaNepal <159408073+NityaNepal@users.noreply.github.com>
2024-02-26 13:50:25 +0600

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,23 @@
+#example 1
+x = 10
+print(x > 3 and x == 10)
+
+#Example 2
+y = 5
+print(y>10 and y % 5 == 0)
+ #Or operator
+# Example 1
+x= 4
+print(x < 3 or x > 10)
+# Example 2
+y = 12
+print(y < 10 or y % 2 == 0)
+
+# Not operator
+# Example 1
+x = 5
+print(not(x > 3 and x < 10))
+
+# Example 2
+y = 12
+print(not(y > 10 and y % 5 == 0))
